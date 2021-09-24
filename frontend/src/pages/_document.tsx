bc445836594462524b348bf2716888284ad59d9f
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-09-24 15:08:43 +0200

    Add fonts preload

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,45 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import type { DocumentContext } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            href="/fonts/1.woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            href="/fonts/opensans400.woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            href="/fonts/opensans800.woff2"
+            crossOrigin="anonymous"
+          />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
