df031b87c81819d200d99e526c180a95efb14981
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-09-24 15:39:08 +0200

    Add canvas app

---
+++ frontend/global.d.ts
@@ -0,0 +1,16 @@
+declare module "prefix";
+
+declare module "*.glsl" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.glb" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
+import { App } from "../classes/App";
+import styles from "../styles/app.module.scss";
 import "../styles/index.scss";
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
   const router = useRouter();
 
+  const rendererWrapperEl = useRef(null);
+  const myApp = useRef<App | null>(null);
+
+  useEffect(() => {
+    if (!rendererWrapperEl.current) return;
+
+    if (rendererWrapperEl.current) {
+      myApp.current = App.getInstance();
+      myApp.current.rendererWrapperEl = rendererWrapperEl.current;
+    }
+
+    return () => {
+      if (myApp.current) myApp.current.destroy();
+    };
+  }, []);
+
   return (
     <>
+      <div className={styles.canvasWrapper} ref={rendererWrapperEl}></div>
       <Component router={router} {...pageProps} />
     </>
   );
