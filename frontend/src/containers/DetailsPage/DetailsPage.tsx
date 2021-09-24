e755b4fd93fee19c699db5675fc62c539d2316d0
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-09-24 15:50:46 +0200

    Add details page

---
+++ frontend/src/containers/DetailsPage/DetailsPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "./detailsPage.module.scss";
+
+export default function DetailsPage() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Details page</h1>
+
+        <Link passHref href="/">
+          <a className={styles.button}>Go to index</a>
+        </Link>
+      </div>
+    </>
+  );
+}
--- frontend/src/containers/IndexPage/IndexPage.tsx
+++ frontend/src/containers/IndexPage/IndexPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import styles from "./indexPage.module.scss";
 
@@ -6,9 +7,13 @@ export default function IndexPage() {
   return (
     <>
       <div className={styles.container}>
-        <h1 className={styles.title}>First page </h1>
+        <h1 className={styles.title}>Index page </h1>
 
         <input className={styles.input} type="text" />
+
+        <Link passHref href="/details">
+          <a className={styles.button}>Go to details</a>
+        </Link>
       </div>
     </>
   );
