ea60fc7bfa5e6edc44246dd173d8afc237bacc8c
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-09-24 16:59:22 +0200

    Add delayed route change

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import React, { useRef, useEffect } from "react";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
+import { useWarningOnExit } from "hooks/useWarningOnExit";
+
 import { App } from "../classes/App";
 import styles from "../styles/app.module.scss";
 import "../styles/index.scss";
@@ -9,6 +11,8 @@ import "../styles/index.scss";
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
   const router = useRouter();
+
+  useWarningOnExit(true);
 
   const rendererWrapperEl = useRef(null);
   const myApp = useRef<App | null>(null);
---
+++ frontend/src/utils/globalState.ts
@@ -0,0 +1,3 @@
+export const globalState = {
+  isTransitioning: false,
+};
