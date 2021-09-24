ad7bf20fbd615226ae27dac430dd7a28ae5718fd
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-09-24 11:48:01 +0200

    Update plugins

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,49 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const path = require("path");
+const withPlugins = require("next-compose-plugins");
+
+const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "./src/styles/responsive.scss";`,
+  },
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/sounds/",
+          outputPath: "static/sounds/",
+          name: "[name].[ext]",
+          esModule: false,
+        },
+      },
+    });
+
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/images",
+          outputPath: "static/images/",
+        },
+      },
+    });
+
+    return config;
+  },
 };
+
+module.exports = withPlugins([], nextConfig);
