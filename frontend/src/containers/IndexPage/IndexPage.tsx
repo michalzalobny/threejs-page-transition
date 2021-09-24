316e67896fa573d0f290175c548be5431623afda
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-09-24 11:32:52 +0200

    Update scss

--- frontend/src/containers/IndexPage/IndexPage.tsx
+++ frontend/src/containers/IndexPage/IndexPage.tsx
@@ -5,17 +5,19 @@ import styles from "./indexPage.module.scss";
 export default function IndexPage() {
   return (
     <>
-      <h1 className={styles.title}>First page </h1>
-      <p>Test</p>
-      <a href="/">test</a>
-      <button
-        onClick={() => {
-          alert("alert");
-        }}
-      >
-        Click me
-      </button>
-      <input className={styles.input} type="text" />
+      <div className={styles.container}>
+        <h1 className={styles.title}>First page </h1>
+        <p>Test</p>
+        <a href="/">test</a>
+        <button
+          onClick={() => {
+            alert("alert");
+          }}
+        >
+          Click me
+        </button>
+        <input className={styles.input} type="text" />
+      </div>
     </>
   );
 }
