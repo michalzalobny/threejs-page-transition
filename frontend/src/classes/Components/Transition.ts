e2a11c99e035fba94bd3718d1749c6acf26a64ba
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-10-09 18:15:56 +0200

    Update transition time

--- frontend/src/classes/Components/Transition.ts
+++ frontend/src/classes/Components/Transition.ts
@@ -31,7 +31,7 @@ export class Transition {
     this._curtainProgressTween = new TWEEN.Tween({
       progress: this._curtainProgress,
     })
-      .to({ progress: destination }, 1200)
+      .to({ progress: destination }, 1000)
       .easing(TWEEN.Easing.Exponential.InOut)
       .onUpdate((obj) => {
         this._curtainProgress = obj.progress;
