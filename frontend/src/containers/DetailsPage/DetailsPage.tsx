6ed5a7fac899f0b94d86233da86b703616820a34
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-10-09 18:30:12 +0200

    Add animation to titles

--- frontend/src/containers/DetailsPage/DetailsPage.tsx
+++ frontend/src/containers/DetailsPage/DetailsPage.tsx
@@ -5,7 +5,9 @@ export default function DetailsPage() {
   return (
     <>
       <div className="details__wrapper">
-        <h1 className="details__title">Details page</h1>
+        <h1 data-animation="paragraph" className="details__title">
+          Details page
+        </h1>
 
         <Link passHref href="/">
           <a className="details__button">Go to index</a>
--- frontend/src/containers/IndexPage/IndexPage.tsx
+++ frontend/src/containers/IndexPage/IndexPage.tsx
@@ -5,7 +5,9 @@ export default function IndexPage() {
   return (
     <>
       <div className="index__wrapper">
-        <h1 className="index__title">Index page 2</h1>
+        <h1 data-animation="paragraph" className="index__title">
+          Index page 2
+        </h1>
 
         <Link passHref href="/details">
           <a className="index__button">Go to details</a>
