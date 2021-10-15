66cdfe137bc46d425f55c2deb328a609be5d71d6
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-10-15 19:29:19 +0200

    Add image to detials

--- frontend/src/containers/DetailsPage/DetailsPage.tsx
+++ frontend/src/containers/DetailsPage/DetailsPage.tsx
@@ -1,30 +1,62 @@
 import React from 'react';
 import Link from 'next/link';
 
+import imageSrc from './images/1.jpg';
+
 export default function DetailsPage() {
   return (
     <>
       <div className="details__wrapper">
-        <h1 data-animation="paragraph" className="details__title">
-          Details page
-        </h1>
-
-        <Link passHref href="/">
-          <a className="details__button">Go to index</a>
+        <Link passHref href="/details">
+          <a className="details__img-link">
+            <figure
+              data-animation="image3d"
+              data-src={imageSrc.src}
+              className="details__img-wrapper"
+            >
+              <img
+                src={imageSrc.src}
+                className="details__img"
+                alt="Newcastle tyne river"
+              />
+            </figure>
+          </a>
         </Link>
-        <div className="details__spacer" />
-        <p data-animation="paragraph" className="details__p">
-          Details page Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Quaerat, dolores excepturi repellendus voluptate nemo, fugiat, a sint
-          cumque quos minima numquam quam recusandae odio facilis optio impedit
-          vero et dolorem?
-        </p>
 
-        <p data-animation="paragraph" className="details__p">
-          Details page Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Quaerat, dolores excepturi repellendus voluptate nemo, fugiat, a sint
-          cumque.
-        </p>
+        <div className="details__container">
+          <h1 data-animation="paragraph" className="details__title">
+            Details page
+          </h1>
+
+          <Link passHref href="/">
+            <a className="details__button">Go to index</a>
+          </Link>
+          <div className="details__spacer" />
+          <p data-animation="paragraph" className="details__p">
+            Details page Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Quaerat, dolores excepturi repellendus voluptate nemo, fugiat,
+            a sint cumque quos minima numquam quam recusandae odio facilis optio
+            impedit vero et dolorem?
+          </p>
+
+          <p data-animation="paragraph" className="details__p">
+            Details page Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Quaerat, dolores excepturi repellendus voluptate nemo, fugiat,
+            a sint cumque.
+          </p>
+
+          <p data-animation="paragraph" className="details__p">
+            Details page Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Quaerat, dolores excepturi repellendus voluptate nemo, fugiat,
+            a sint cumque.
+          </p>
+
+          <p data-animation="paragraph" className="details__p">
+            Details page Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Quaerat, dolores excepturi repellendus voluptate nemo, fugiat,
+            a sint cumque.
+          </p>
+        </div>
       </div>
     </>
   );
