ab21fca7eb9671ff7979723dfc372b38ad2324ee
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-10-16 23:06:08 +0200

    Update details page

--- frontend/src/containers/DetailsPage/DetailsPage.tsx
+++ frontend/src/containers/DetailsPage/DetailsPage.tsx
@@ -7,28 +7,31 @@ export default function DetailsPage() {
   return (
     <>
       <div className="details__wrapper">
-        <div style={{ height: '10vh' }}></div>
-        <figure
-          data-transition="details"
-          data-animation="image3d"
-          data-src={imageSrc.src}
-          className="details__img-wrapper"
-        >
-          <img
-            src={imageSrc.src}
-            className="details__img"
-            alt="Newcastle tyne river"
-          />
-        </figure>
+        <Link passHref href="/">
+          <a className="details__img-link">
+            <figure
+              data-transition="details"
+              data-animation="image3d"
+              data-src={imageSrc.src}
+              className="details__img-wrapper"
+            >
+              <img
+                src={imageSrc.src}
+                className="details__img"
+                alt="Newcastle tyne river"
+              />
+            </figure>
+          </a>
+        </Link>
 
         <div className="details__container">
           <h1 data-animation="paragraph" className="details__title">
             Details page
           </h1>
 
-          <Link passHref href="/">
+          {/* <Link passHref href="/">
             <a className="details__button">Go to index</a>
-          </Link>
+          </Link> */}
           <div className="details__spacer" />
           <p data-animation="paragraph" className="details__p">
             Details page Lorem ipsum dolor sit amet consectetur, adipisicing
