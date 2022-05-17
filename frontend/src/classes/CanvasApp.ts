d4e78873423d4e81d1de9eea9af0b9ccbf9aaf3d
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-05-18 01:20:29 +0200

    Fix initial hover state issue

--- frontend/src/classes/CanvasApp.ts
+++ frontend/src/classes/CanvasApp.ts
@@ -109,6 +109,17 @@ export class CanvasApp extends THREE.EventDispatcher {
   _onAssetsLoaded = (e: THREE.Event) => {
     globalState.textureItems = (e.target as Preloader).textureItems;
     this._animateGlobalOpacity();
+
+    const pageWrapper = Array.from(
+      document.querySelectorAll('.page-wrapper'),
+    )[0] as HTMLElement;
+
+    const pageOverlay = Array.from(
+      document.querySelectorAll('.page__overlay'),
+    )[0] as HTMLElement;
+
+    pageWrapper.classList.add('page-wrapper--active');
+    pageOverlay.classList.add('page__overlay--disabled');
     this._pageManager.onAssetsLoaded();
   };
 
@@ -191,17 +202,6 @@ export class CanvasApp extends THREE.EventDispatcher {
     const page = Array.from(
       document.querySelectorAll(`[data-pageid="${globalState.currentPageId}"]`),
     )[0] as HTMLElement;
-
-    const pageWrapper = Array.from(
-      document.querySelectorAll('.page-wrapper'),
-    )[0] as HTMLElement;
-
-    const pageOverlay = Array.from(
-      document.querySelectorAll('.page__overlay'),
-    )[0] as HTMLElement;
-
-    pageWrapper.classList.add('page-wrapper--active');
-    pageOverlay.classList.add('page__overlay--disabled');
 
     this._onResize();
     this.handlePageEnter(page);
